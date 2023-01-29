1f40aa8b11f299d830d4b1445e930d326215ec0c
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-01-28 21:51:17 -0300

    Feito a validação do botão Enviar, com estilos para os botões e mais ações no script. Inclusão de uma div para a mensagem de sucesso.

--- assets/js/recuperar_senha.js
+++ assets/js/recuperar_senha.js
@@ -1,5 +1,29 @@
 // Preenchimento do Campo aparece a partir da opção selecionada
 
-$('#porEmail').on('change', () => { $('#displayEmail').removeClass('visually-hidden'); $('#displaySMS').addClass('visually-hidden'); $(':submit').removeClass('visually-hidden')})
-$('#porSMS').on('change', () => { $('#displaySMS').removeClass('visually-hidden'); $('#displayEmail').addClass('visually-hidden'); $(':submit').removeClass('visually-hidden')})
+$('#porEmail').on('change', whichIsActive)
+$('#porSMS').on('change', whichIsActive)
 
+function whichIsActive() {
+    if ($('#porEmail:checked').val() == 'on') {
+        $('#displayEmail').removeClass('visually-hidden'); 
+        $('#displaySMS').addClass('visually-hidden'); 
+        $(':submit').removeClass('visually-hidden');
+
+        if($('#email').val() == '') {
+            disBotao()
+        } else {
+            atvBotao()
+        }
+    }
+    else {
+        $('#displaySMS').removeClass('visually-hidden'); 
+        $('#displayEmail').addClass('visually-hidden'); 
+        $(':submit').removeClass('visually-hidden');
+
+        if($('#cel').val() == '') {
+            disBotao()
+        } else {
+            atvBotao()
+        }
+    }
+}
--- assets/js/valida.js
+++ assets/js/valida.js
@@ -1,3 +1,6 @@
+// Botão ativado somente se os campos estiverem ok
+const atvBotao = () => $('.btn').removeAttr('disabled');
+const disBotao = () => $('.btn').prop('disabled', true)
 // Validação do E-mail
 
 const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
@@ -11,9 +14,11 @@ function testEmail() {
     if (regexEmail.test(emailVal)) {
         campoEmail.next().html('&nbsp;')
         campoEmail.css({'border':''})
+        atvBotao()
     } else {
         campoEmail.next().text('E-mail inválido')
         campoEmail.css({'border':'1px solid red'})
+        disBotao()
     }
 }
 
@@ -29,8 +34,24 @@ function testCel () {
     if (regexTel.test(celVal)) {
         campoCel.next().html('&nbsp;')
         campoCel.css({'border':''})
+        atvBotao()
     } else {
         campoCel.next().text('Número de celular inválido')
         campoCel.css({'border':'1px solid red'})
+        disBotao()
     }
 }
+
+// Confirmar o envio
+$('.btn').on('click', () => {
+    $('.successAlert').css({
+        'right': '1rem'
+    })
+
+    $('.timebar').animate({'width': '0%'}, 5000)
+    setTimeout(()=>{
+        $('.successAlert').css({'right':''})
+        $('.timebar').css({'width': '100%'})
+    }, 5000)
+    disBotao()
+})
